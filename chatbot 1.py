1d4cecdece8512d6e175e5525fca85e5c3268e91
pietrozimbardo2-max <pietrozimbardo2@gmail.com>
2026-05-18 14:55:48 +0200

    Update chatbot 1.py

--- chatbot 1.py
+++ chatbot 1.py
@@ -88,7 +88,7 @@ if documento is not None:
         model="gpt-5.4-nano",
         temperature=0.3,
         max_tokens=1000,
-        openai_api_key= st.secrets["OPENAI_API_KEY"]
+        openai_api_key= st.secrets["OPENAI_API_KEY"])
     
     catena = (
          {"context": comparatore | formatta_documento, 
